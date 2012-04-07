e08a07fcd20370f5804146135f1e642625fb4a04
Gabriel Horner <gabriel.horner@gmail.com>
2012-04-07 18:44:31 -0400

    allow m to run specs

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,5 +1,6 @@
 gem 'minitest' unless ENV['NO_RUBYGEMS']
-require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/autorun' unless $0.end_with?('/m')
 require 'urls'
 require 'fileutils'
 require 'bahia'
